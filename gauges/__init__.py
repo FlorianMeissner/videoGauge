a343605e13c0292aaf6cf9b3e7e0acbf6a700983
Florian Meissner <florianmeissner@gmx.de>
2017-02-26 21:21:20 +0100

    Upload compass, G-Meter, gyro, tachometer and vertical speed indicator. So far only graphics exist to be converted for use with  videoGauge.

---
+++ gauges/__init__.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+# *************************************************************************************************
+# * Gauges for Video Gauge Creator                                                                *
+# *************************************************************************************************
+
+
+# Description
+# ===========
+
+# Collection of gauge templates used for Video gauge Creator. To add new templates, create a
+# subfolder according to the following structure.
+
+# <VideoGauge>
+#   gauges
+#       gauge1
+#           faceplate.png
+#           needle.png
+#           __init__.py
+#       gauge2
+#           ...
+
+# Look at the existing gauges for further information.
+
+# At last, add the subfolder and gauge class to the end of this file.
+
+
+# TODO
+# ====
+
+# -
+
+
+# ABOUT
+# =====
+
+# Creator:  Florian Meissner
+#           n1990b@gmx.de
+# Version:  0.1
+# Date:     2017/02/21
+
+
+# VERSION HISTORY
+# ===============
+
+# 0.1:  Initial Beta
+
+
+###################################################################################################
+
+
+#from airspeed_luscombe_mph import AirspeedLuscombeMPH
+
+
+# EOF
